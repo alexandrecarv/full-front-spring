3215b94ddb9d61424e12f5b6d5bc75286fa23403
Alexandre Carvalho <99741779+alexandrecarv@users.noreply.github.com>
2022-06-06 18:09:55 -0300

    adicionado o front

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { CreateEmployeeComponent } from './componentes/create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './componentes/employee-details/employee-details.component';
+import { EmployeeListComponent } from './componentes/employee-list/employee-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UpdateEmployeeComponent } from './componentes/update-employee/update-employee.component';
+import { EmployeeService } from './service/employee.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
